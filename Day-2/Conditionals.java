3b4ca9a90efe331a3eb952d0601e572bfb852b06
SwethaR2706 <swetharavuri27@gmail.com>
2025-06-11 13:12:28 +0000

    red

---
+++ Day-2/Conditionals.java
@@ -0,0 +1,22 @@
+package Training;
+
+public class Conditionals {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("Hello, World!");
+		String signal_color="red";
+		
+		if (signal_color=="red")
+			System.out.println("Wait");
+		else if(signal_color=="green")
+			System.out.println("Go");
+		else if(signal_color=="orange")
+			System.out.println("Slow down");
+		else
+			System.out.println("Invalid Input");
+
+	}
+
+}
+
