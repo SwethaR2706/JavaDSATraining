2293397a0a2a97aa56f7d20941de72a9c96f94e5
SwethaR2706 <swetharavuri27@gmail.com>
2025-06-11 13:15:19 +0000

    day

---
+++ Day-2/SwitchCase.java
@@ -0,0 +1,26 @@
+package Training;
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int day=3;
+		switch(day) {
+		case 1:
+			System.out.println("Monday");
+			break;
+		case 2:
+			System.out.println("Tuesday");
+			break;
+		case 3:
+			System.out.println("Wednesday");
+			break;
+		case 4:
+			System.out.println("Thursday");
+			break;
+		default:
+			System.out.println("Invalid input");
+		}
+	}
+
+}
