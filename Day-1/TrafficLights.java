304361693ffe9036903cf4221152e651502ffcbf
SwethaR2706 <swetharavuri27@gmail.com>
2025-06-11 13:00:05 +0000

    red

---
+++ Day-1/TrafficLights.java
@@ -0,0 +1,18 @@
+package Training;
+
+public class TrafficLights {
+
+	public static void main(String[] args) {
+		String color="red";
+		if(color=="red")
+			System.out.println("Stop");
+		else if(color=="green")
+			System.out.println("Go");
+		else if(color=="orange")
+			System.out.println("Wait");
+		else
+			System.out.println("Invalid input");
+	}
+
+}
+
