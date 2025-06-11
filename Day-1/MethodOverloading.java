c6488f4241da8627512b9047543883633e8945c0
SwethaR2706 <swetharavuri27@gmail.com>
2025-06-11 13:02:41 +0000

    a

---
+++ Day-1/MethodOverloading.java
@@ -0,0 +1,16 @@
+package Training;
+
+public class MethodOverloading {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello,World!");
+	}
+	public static void main(int[] args) {
+		int a=10;
+		int b=20;
+		int sum=a+b;
+		System.out.println("Sum = "+sum);
+	}
+
+}
