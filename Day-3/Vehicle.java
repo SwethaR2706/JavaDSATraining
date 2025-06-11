bb9ad26be3dd486e0c5beb615b15e3d48daf7737
SwethaR2706 <swetharavuri27@gmail.com>
2025-06-11 13:10:13 +0000

    car

---
+++ Day-3/Vehicle.java
@@ -0,0 +1,14 @@
+package Training;
+
+public class Vehicle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Vehicle v=new Vehicle();
+		v.drivingCar();
+	}
+	public void drivingCar() {
+		System.out.println("Driving the Car");
+	}
+
+}
