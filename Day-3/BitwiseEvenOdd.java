e11d8687d2781fa2e0dc476bb62d6ace05cf41dc
SwethaR2706 <swetharavuri27@gmail.com>
2025-06-11 13:04:20 +0000

    &

---
+++ Day-3/BitwiseEvenOdd.java
@@ -0,0 +1,14 @@
+package Training;
+
+public class BitwiseEenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=4;
+		if((num&1)==0)
+			System.out.println("Even");
+		else
+			System.out.println("Odd");
+	}
+
+}
