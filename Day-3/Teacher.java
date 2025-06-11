82be7f94dbdee071cfc05bbc0e759792d26dbebb
SwethaR2706 <swetharavuri27@gmail.com>
2025-06-11 13:08:58 +0000

    a

---
+++ Day-3/Teacher.java
@@ -0,0 +1,23 @@
+package Training;
+
+public class Teacher {
+	String teacherName,subject;
+
+	public String getTeacherName() {
+		return teacherName;
+	}
+
+	public void setTeacherName(String teacherName) {
+		this.teacherName = teacherName;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	
+	
+}
