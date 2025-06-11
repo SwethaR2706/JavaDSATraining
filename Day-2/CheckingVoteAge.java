fd2cce2d7448662cb478cbecbc9d6d6bb86f1c06
SwethaR2706 <swetharavuri27@gmail.com>
2025-06-11 13:11:46 +0000

    age

---
+++ Day-2/CheckingVoteAge.java
@@ -0,0 +1,17 @@
+package Training;
+
+public class CheckVotingAge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age=16;
+		
+		if(age>=18) {
+			System.out.println("Eligible to vote");
+		}
+		else {
+			System.out.println("Not Eligible to vote");
+		}
+	}
+
+}
