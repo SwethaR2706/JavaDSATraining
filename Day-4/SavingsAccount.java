ee47117dcee7683b6b756dfbf5277befebfe01a7
SwethaR2706 <swetharavuri27@gmail.com>
2025-06-12 05:56:29 +0000

    acc

---
+++ Day-4/SavingsAccount.java
@@ -0,0 +1,18 @@
+package Training;
+
+public class SavingsAccount extends Account {
+	
+	final double MIN_BAL=0.0;
+	public SavingsAccount(String name,String pan,String mobNum,long aadhar,String type,double bal,double total) {
+		super(name,pan,mobNum,aadhar,type,bal,total);
+	}
+	@Override
+	public double withDraw(double amount) {
+		if(totalBal>MIN_BAL)
+			totalBal-=amount;
+		else
+			System.out.println("Insfficient Balance....");
+		return totalBal;
+	}
+
+}
