6188c4c0412e68d7be7c89134c685d02ec7707ec
SwethaR2706 <swetharavuri27@gmail.com>
2025-06-12 09:21:47 +0000

    x

---
+++ Day-4/CalculatorImpl.java
@@ -0,0 +1,16 @@
+package Training;
+
+public class CalculatorImpl implements Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Calculator.log("Hello");
+			System.out.println(Calculator.createEmail("Swetha", "Sri"));
+		}
+		@Override
+		public double add() {
+			return 0;
+		}
+
+}
+
