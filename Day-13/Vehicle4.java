58388d4945b35fec7c6b80467730b0306bb4e46f
SwethaR2706 <swetharavuri27@gmail.com>
2025-06-24 04:25:50 +0000

    drive

---
+++ Day-13/Vehicle4.java
@@ -0,0 +1,22 @@
+package Training;
+
+public class vehicle4 implements Myinterface {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		vehicle4 v1=new vehicle4();
+		v1.start();
+		v1.stop();
+		v1.drive();
+	}
+	public void start() {
+		
+	}
+	public void stop() {
+		
+	}
+	public void drive() {
+		
+	}
+
+}
